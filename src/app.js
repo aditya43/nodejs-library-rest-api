5559bdee57f39f59ea1fa7a436168ea77e7934e5
Aditya Hajare <aditya43@gmail.com>
2020-07-15 00:09:03 +0530

    Skeleton created

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 require('./config/loadEnv');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+
+const morganLogger = require('./middleware/morganLogger');
+// const authorRoutes = require('./routes/authors');
+// const bookRoutes = require('./routes/books');
+
+const app = express();
+app.use(helmet());
+app.use(morganLogger);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use(authorRoutes);
+// app.use(bookRoutes);
+
+module.exports = app;
