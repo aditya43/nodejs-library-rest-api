46e3dd3cea533124d3a71c342697cf83914689e4
Aditya Hajare <aditya43@gmail.com>
2020-07-15 08:59:57 +0530

    Author login

--- src/controllers/author.js
+++ src/controllers/author.js
@@ -102,3 +102,17 @@ exports.delete = async (req, res) => {
         res.status(400).send(e);
     }
 };
+
+/**
+ * Author login
+ */
+exports.login = async (req, res) => {
+    try {
+        const author = await Author.findByCredentials(req.body.email, req.body.password);
+        const jwtToken = await author.generateJwtAuthToken();
+
+        res.status(200).send({ author, jwtToken });
+    } catch (e) {
+        res.status(400).send({ error: 'Invalid credentials' });
+    }
+};
