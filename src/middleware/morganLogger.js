1d59de2c2411116cbdef4a363f6f3168450de910
Aditya Hajare <aditya43@gmail.com>
2020-07-15 09:07:29 +0530

    Middleware to generate access logs

--- src/middleware/morganLogger.js
+++ src/middleware/morganLogger.js
@@ -2,6 +2,9 @@ const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * Middleware to generate access logs
+ */
 const accessLogStream = fs.createWriteStream(path.join(__dirname, '../../logs/access.log'), { flags: 'a' });
 
 module.exports = morgan('combined', { stream: accessLogStream });
