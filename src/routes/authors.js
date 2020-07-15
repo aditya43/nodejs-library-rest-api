c12d12c8488e63eea71639aa3027e7fa279ed069
Aditya Hajare <aditya43@gmail.com>
2020-07-15 08:50:12 +0530

    Delete author account

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -10,7 +10,7 @@ router.get('/authors/books', authMiddleware, authorController.myBooks);
 router.get('/authors/:id', authorController.get);
 router.post('/authors/login', authorController.login);
 router.patch('/authors/me', authMiddleware, authorController.update);
-// router.delete('/authors/me', authMiddleware, authorController.delete);
+router.delete('/authors/me', authMiddleware, authorController.delete);
 // router.post('/authors/search', authMiddleware, authorController.search);
 router.post('/authors/logout', authMiddleware, authorController.logout);
 router.post('/authors/logoutAll', authMiddleware, authorController.logoutAll);
