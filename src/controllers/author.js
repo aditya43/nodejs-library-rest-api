e9d274080ae5c9b2d61fe27531e49727439226e7
Aditya Hajare <aditya43@gmail.com>
2020-07-15 08:35:58 +0530

    Create author | Register new account for author

--- src/controllers/author.js
+++ src/controllers/author.js
@@ -6,5 +6,13 @@ const Author = require('../models/author');
  * Create/Add new author
  */
 exports.add = async (req, res) => {
-    //
+    try {
+        const author = new Author(req.body);
+        await author.save();
+        const jwtToken = await author.generateJwtAuthToken();
+
+        res.status(201).send({ author, jwtToken });
+    } catch (e) {
+        res.status(400).send(e);
+    }
 };
