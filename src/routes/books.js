4999cad5ac6573cf247dad4ac93181544423c8fd
Aditya Hajare <aditya43@gmail.com>
2020-07-15 08:52:45 +0530

    Add/Create new book

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,28 +1,21 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const bookController = require('../controllers/book');
+const authMiddleware = require('../middleware/auth');
 
-const bookSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    isbn: {
-        type: Number,
-        required: true
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Author',
-        trim: true,
-        required: true
-    },
-    releaseDate: {
-        type: Date
-    }
-}, {
-    timestamps: true
-});
+const router = new express.Router();
 
-const Book = mongoose.model('Book', bookSchema);
+router.post('/books', authMiddleware, bookController.add);
+// router.post('/books/search', authMiddleware, bookController.search);
 
-module.exports = Book;
+// GET /books?limit=10&skip=10
+// GET /books?sortBy=createdAt:asc
+// GET /books?sortBy=createdAt:desc
+// GET /books?sortBy=releaseDate:asc
+// GET /books?sortBy=releaseDate:desc
+// router.get('/books/me', authMiddleware, bookController.myBooks);
+
+// router.get('/books/:id', authMiddleware, bookController.get);
+// router.patch('/books/:id', authMiddleware, bookController.update);
+// router.delete('/books/:id', authMiddleware, bookController.delete);
+
+module.exports = router;
