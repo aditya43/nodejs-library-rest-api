64e3cbc7ddd9b884413c0c8227b63b0f828dd7ee
Aditya Hajare <aditya43@gmail.com>
2020-07-15 08:48:12 +0530

    Implemented logout all sessions route on author

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -1,121 +1,18 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const express = require('express');
+const authorController = require('../controllers/author');
+const authMiddleware = require('../middleware/auth');
 
-const Book = require('../models/book');
+const router = new express.Router();
 
-const authorSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate (value) {
-            if (value < 0) {
-                throw new Error('Age must be positive number');
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        validate (value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Invalid email address');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate (value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error(`Password cannot contain the word 'password'`);
-            }
-        }
-    },
-    tokens: [{
-        token: {
-            type: String,
-            required: true
-        }
-    }]
-}, {
-    timestamps: true
-});
+router.post('/authors', authorController.add);
+// router.get('/authors/me', authMiddleware, authorController.myProfile);
+// router.get('/authors/books', authMiddleware, authorController.myBooks);
+// router.get('/authors/:id', authorController.get);
+router.post('/authors/login', authorController.login);
+// router.patch('/authors/me', authMiddleware, authorController.update);
+// router.delete('/authors/me', authMiddleware, authorController.delete);
+// router.post('/authors/search', authMiddleware, authorController.search);
+router.post('/authors/logout', authMiddleware, authorController.logout);
+router.post('/authors/logoutAll', authMiddleware, authorController.logoutAll);
 
-// Author --> Books Relation.
-authorSchema.virtual('books', {
-    ref: 'Book',
-    localField: '_id',
-    foreignField: 'author'
-});
-
-authorSchema.pre('find', function () {
-    this.populate('book');
-});
-
-// methods = 'generateJwtAuthToken' method can be called on an instance of 'Author' model.
-authorSchema.methods.generateJwtAuthToken = async function () {
-    const token = jwt.sign({ _id: this._id.toString() }, process.env.JWT_SECRET);
-    this.tokens = this.tokens.concat({ token }); // Save token to 'Author' model.
-
-    await this.save(); // Save 'Author' model
-
-    return token;
-};
-
-// To remove 'password' and 'tokens' data from 'author' object when returned.
-authorSchema.methods.toJSON = function () {
-    const author = this.toObject();
-
-    delete author.password;
-    delete author.tokens;
-
-    return author;
-};
-
-// statics = 'findByCredentials' method will be accessible for calling on 'Author' model itself.
-authorSchema.statics.findByCredentials = async (email, password) => {
-    const author = await Author.findOne({ email });
-
-    if (!author) {
-        throw new Error('Invalid credentials'); // Author not found for given email
-    }
-
-    const isPasswordMatch = await bcrypt.compare(password, author.password);
-
-    if (!isPasswordMatch) {
-        throw new Error('Invalid credentials'); // Password doesn't match
-    }
-
-    return author;
-};
-
-// Hash the plain text password before saving.
-authorSchema.pre('save', async function (next) { // Using standard function since arrow functions don't bind 'this'.
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 8);
-    }
-
-    next(); // If we don't call 'next()' code will hang here forever and author won't be saved.
-});
-
-// Remove all books owned by author before deleting author account
-authorSchema.pre('remove', async function (next) {
-    await Book.deleteMany({ author: this._id });
-    next();
-});
-
-const Author = mongoose.model('Author', authorSchema);
-
-module.exports = Author;
+module.exports = router;
