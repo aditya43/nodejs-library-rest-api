4fa0f2f36840b9e71089f159ce7d67666bfb0c66
Aditya Hajare <aditya43@gmail.com>
2020-07-15 09:11:28 +0530

    Test | should create book for author

--- tests/book.test.js
+++ tests/book.test.js
@@ -11,3 +11,18 @@ const {
 } = require('./fixtures/db');
 
 beforeEach(populateDatabase);
+
+test('should create book for author', async () => {
+    const response = await request(app)
+        .post('/books')
+        .set('Authorization', `Bearer ${authorOne.tokens[0].token}`)
+        .send({
+            title: 'From Jest test',
+            isbn: '123456789',
+            author: authorOneId
+        })
+        .expect(201);
+
+    const book = await Book.findById(response.body._id);
+    expect(book).not.toBeNull();
+});
